e7c7c0f694b74010423382aa1a94890209f16d13
hrishikesh885 <kavale.hrishikesh@gmail.com>
2022-02-09 19:27:54 +0530

    commit 3

--- app/src/main/java/com/example/linklet/MainActivity.java
+++ app/src/main/java/com/example/linklet/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.linklet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button submitBtn= findViewById(R.id.submit);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkPermission()){
+                    // for permission allowed
+                    Intent intent=new Intent(MainActivity.this, file_activity.class);
+                    String path= Environment.getExternalStorageDirectory().getPath();
+                            intent.putExtra("path",path);
+                            startActivity(intent);
+                }
+                else{
+                    //for permission not allowed
+                    requestPermission();
+                }
+            }
+        });
+    }
+    private boolean checkPermission(){
+        int result=ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(result== PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else
+            return false;
+    }
+    private void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this, "STORAGE PERMISSION IS REQUIRED", Toast.LENGTH_SHORT).show();
+        }
+        else
+        ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},111);
     }
 }
